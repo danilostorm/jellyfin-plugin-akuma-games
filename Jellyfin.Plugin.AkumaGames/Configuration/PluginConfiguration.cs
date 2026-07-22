b259e2fa04eec80c3a3a449df3e8c25485ffaed4
DaNiLoStOrM <storm.danilo@gmail.com>
2026-07-21 21:12:15 -0300

    feat: add plugin configuration

---
+++ Jellyfin.Plugin.AkumaGames/Configuration/PluginConfiguration.cs
@@ -0,0 +1,14 @@
+using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.AkumaGames.Configuration;
+
+public sealed class PluginConfiguration : BasePluginConfiguration
+{
+    public string ApiUrl { get; set; } = "https://play.hoststorm.cloud/api/akumanimes";
+    public string LibraryName { get; set; } = "Games";
+    public bool AutoCreateLibrary { get; set; } = true;
+    public bool SyncOnStartup { get; set; } = true;
+    public bool DownloadImages { get; set; } = true;
+    public bool RemoveMissingGames { get; set; } = true;
+    public int SyncIntervalHours { get; set; } = 12;
+}
